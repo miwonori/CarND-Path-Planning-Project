b52d3f3e0fb5ff6c33bf7ea36e20dc2712c3411d
eschoi <miwonori@gmail.com>
2021-08-29 21:14:08 +0900

    edit code

--- data/check_data.m
+++ data/check_data.m
@@ -1,13 +1,25 @@
 T = readtable('highway_map.csv');
-%%
+index = 0:1:length(T.Var1)-1;
+%% Cartensian Coordinate
 figure;hold on;
 plot(T.Var1,T.Var2,'.-');
+text(T.Var1,T.Var2,num2str(index'));
 plot(T.Var1(1),T.Var2(2),'or');
 text(T.Var1(1),T.Var2(2),'Start');
 
- X = 909.48; Y = 1128.67;
-plot(X,Y,'dm');
-text(X,Y,'Car Position');
+start_X = 909.48; start_Y = 1128.67;
+plot(start_X,start_Y,'dm');
+text(start_X,start_Y,'Car Position');
+
+%%  Frenet Coordinate
+figure;hold on;
+% plot(T.Var4,T.Var3,'.-');
+plot(T.Var4+6,T.Var3,'.-');
+text(T.Var4+6,T.Var3,num2str(index'));
+
+start_S = 124.834; start_D = 6.16483;
+plot(start_D,start_S,'dm');
+text(start_D,start_S,'Car Position');
 
 %%
  X = 915.881;  Y = 1126.89;
@@ -42,4 +54,10 @@ plot(X,Y,'ok');
   plot(X,Y,'ok');
   
   %%
+X = 1080.96; Y = 1174.44; plot(X,Y,'ok');
+X = 1081.35; Y = 1174.58; plot(X,Y,'om');
+X = 1080.86; Y = 1174.41; plot(X,Y,'ok');
+X = 1081.25; Y = 1174.55; plot(X,Y,'om');
+X = 1103.5; Y = 1180.03; plot(X,Y,'ok');
+X = 1127.92; Y = 1183.17; plot(X,Y,'om');
   
--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,7 @@ int main() {
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
+  int index = 0;
 
   std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
@@ -61,12 +62,12 @@ int main() {
   std::chrono::system_clock::time_point newTime;
   std::chrono::duration<double> delTime;
   bool first_flag = true;
-  double oldSpeed;
+  double car_speed_old;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy,
                &oldTime, &newTime, &delTime, &first_flag,
-               &oldSpeed]
+               &car_speed_old, &index]
               (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -113,10 +114,11 @@ int main() {
            *   sequentially every .02 seconds
            */
           double lane_width = 4.0;
+          double Max_speed = 45;    // MPH
           
           if (first_flag) {
             first_flag = 0;
-            oldSpeed = car_speed;
+            car_speed_old = car_speed;
             newTime = std::chrono::system_clock::now();
           }
 
@@ -127,150 +129,115 @@ int main() {
           // cout << "Time Interval = " << delTime.count() << endl;
 
           /* Current Position in Frenet Coordinate */
-          // cout << "Car Pos X = " << car_x << ", Car Pos Y = " << car_y << ", Car Pos Yaw = " << car_yaw << endl;
-          // cout << "Car S = " << car_s << ", Car D = "<< car_d << endl;
-          int current_lane_N = ceil(car_d/4);
+          // cout << "Car Pos X = " << car_x << ", Car Pos Y = " << car_y << ", Car Pos Yaw = " << car_yaw << "Car S = " << car_s << ", Car D = "<< car_d << endl;
+          if (car_speed >= Max_speed){
+            cout << "        Car Speed = " << car_speed << endl;
+          }
+          
+          int current_lane_N = ceil(car_d/4.0);
           // cout << "Current Lane Number =" << current_lane_N << endl;
 
           int WP_N ;
           WP_N = NextWaypoint(car_x, car_y, deg2rad(car_yaw), map_waypoints_x, map_waypoints_y);
-          // cout << "WayPoint N = " << WP_N << ", Next WayPoint S =" << map_waypoints_s[WP_N] << endl;
+          // cout << "Car S = " << car_s << ", WayPoint N = " << WP_N << ", Next WayPoint S =" << map_waypoints_s[WP_N] << endl;
 
           /* if next way point is closed increas waypoint number */
           double dist_to_WP = map_waypoints_s[WP_N] - car_s ;
-          if (dist_to_WP < 5.0){WP_N += 1;}
+          if (dist_to_WP < 2.0){WP_N += 1;}
           // cout << "Distance del S : " << map_waypoints_s[WP_N] - car_s << endl;
 
           int path_size = previous_path_x.size();
-          std::cout << "previous Path_size = " << path_size << std::endl;
+          // std::cout << "previous Path_size = " << path_size << std::endl;
           
           /* Find Spline Path */
           vector<double> X;
           vector<double> Y;
 
-          int num_of_mid_point = 3;
-          double next_WP_s = map_waypoints_s[WP_N];
-          double del_s = (next_WP_s - car_s)/(num_of_mid_point + 1);
-          double mid_s;
+          // int num_of_mid_point = 3;
+          // double next_WP_s = map_waypoints_s[WP_N];
+          // double del_s = (next_WP_s - car_s)/(num_of_mid_point + 1);
+          double mid_s = car_s;
           double mid_d;
           vector<double> mid_XY;
           vector<double> mid_SD;
+          int use_size = 4 ;
           
           X.push_back(car_x);
           Y.push_back(car_y);
+
           mid_d = lane_width/2 + (lane_width * (current_lane_N-1));
+
           if (path_size != 0){
-            for (int i = 0; i < path_size; i++){
-              mid_SD = getFrenet(previous_path_x[i], previous_path_y[i], deg2rad(car_yaw), 
-                                 map_waypoints_x, map_waypoints_y);
-              if (mid_SD[0] < next_WP_s){
+            for (int i = 0; i < use_size; i++){
+              mid_SD = getFrenet(previous_path_x[i], previous_path_y[i], deg2rad(car_yaw), map_waypoints_x, map_waypoints_y);
+              if (mid_SD[0] > mid_s){
                 X.push_back(previous_path_x[i]);
                 Y.push_back(previous_path_y[i]);
-              } else {
-                break;
+                mid_s = mid_SD[0];
               }
-            }
-          } else {
-            for (int i = 0; i < num_of_mid_point; i++){
-              mid_s = car_s + (del_s * (i+1)) ;
-              mid_XY = getXY(mid_s, mid_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
-              X.push_back(mid_XY[0]);
-              Y.push_back(mid_XY[1]);
             }
           }
 
           mid_XY = getXY(map_waypoints_s[WP_N], mid_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
           X.push_back(mid_XY[0]);
           Y.push_back(mid_XY[1]);
-          // cout << "mid_XY X = " << mid_XY[0] << ", mid_XY Y = " << mid_XY[1] << endl;
-          cout << "1" << endl;
+
+          mid_XY = getXY(map_waypoints_s[WP_N+1], mid_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          X.push_back(mid_XY[0]);
+          Y.push_back(mid_XY[1]);
+
+          // for (int i = 0; i <X.size();i++){
+          //   cout << "X = " << X[i] << "; Y = " << Y[i] << endl;
+          // }
+
+          /* Make Spline function */
           tk::spline path_spline(X,Y);
-           cout << "2" << endl;
           double spline_Y;
-
-          double Max_Speed = 45;    //MPH
-          double max_acc = 9;
-          double max_jerk_acc = 9 ; //m/s^3
-          double del_t = 0.02;
           double dist_inc = 0.4;
-          // double acc = MPH2mps(car_speed - oldSpeed)/del_t;
-          // double acc = MPH2mps(car_speed - oldSpeed)/delTime.count();
-          // acc += max_jerk_acc*del_t;
-          // acc += max_jerk_acc*delTime.count();
-          // if (acc > max_acc){
-          //   acc = max_acc;
-          // }
-          // cout << "old spd = " << oldSpeed << ", new spd = " << car_speed << endl;
-          // cout << "Acceleration = " << acc << endl;
 
-          // // dist_inc = 0.4;
-          // // dist_inc = ((MPH2mps(car_speed)*delTime.count()) + (0.5*max_acc*delTime.count()));
-          // if (car_speed < Max_Speed){
-          //   // dist_inc = ((MPH2mps(car_speed)*del_t) + (0.5*acc*del_t*del_t));
-          //   dist_inc = ((MPH2mps(car_speed)*delTime.count()) + (0.5*max_acc*delTime.count()*delTime.count()));
-          // } else {
-          //   dist_inc = (MPH2mps(Max_Speed)*del_t);
-          // }
-          
-          // if (dist_inc > 0.4){
-          //   dist_inc = 0.4;
-          // }
+          use_size = 0;
+          mid_s = car_s;
+          if ((path_size != 0)&&(use_size !=0)){
+            for (int i = 0; i < use_size; i++){
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+              mid_SD = getFrenet(previous_path_x[i], previous_path_y[i], deg2rad(car_yaw), map_waypoints_x, map_waypoints_y);
+              mid_s = mid_SD[0]; 
+            }
+          }
 
-          cout << "Est dist increment :" << dist_inc << endl;
-          for (int i = 0; i < 20; i++){
-            mid_s = car_s + (dist_inc*(i+1)) ;
+          double del_T =  delTime.count() / (50 - path_size);
+          if ((del_T < 0.0001) || isfinite(del_T)){
+            del_T = 0.02;
+          }
+
+          double speed = MPH2mps(car_speed);
+          for (int i = use_size; i < 50; i++){
+            
+            if(speed < MPH2mps(Max_speed)){
+              speed = speed + (10 * del_T) ;
+            }
+
+            if (speed > MPH2mps(Max_speed)){
+              speed = MPH2mps(Max_speed);
+            }
+            dist_inc = speed * del_T;
+            cout << "WayPoint = " << WP_N <<  ", Set Speed = " << speed << ", Set dist = " << dist_inc << ", Del T = " << del_T << endl;
+
+            mid_s = mid_s + dist_inc ;
             mid_XY = getXY(mid_s, mid_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
             spline_Y = path_spline(mid_XY[0]);
-            // if (i == 0){
-            //   cout << "First WayPoint X = " << mid_XY[0] << ", First WayPoint Y = " << spline_Y << endl;
-            // }
-            // cout << " Next X = " << mid_XY[0] << ", Next Y = " << spline_Y << endl;
             next_x_vals.push_back(mid_XY[0]);
             next_y_vals.push_back(spline_Y);
           }
-           cout << "4" << endl;
-          // cout << "Last WayPoint X = " << mid_XY[0] << ", Last WayPoint Y = " << spline_Y << endl;
+          cout << "previous Path_size = " << path_size << " Next Vals Size = " << next_x_vals.size() << endl;
+          // double acc = (car_speed - car_speed_old) / delTime.count() ;
+          // cout << "index: " << index << ", del Vel = " << (car_speed - car_speed_old) << ", delta T = " << delTime.count() << ", Accel = " << acc <<  endl;
+          // cout << "Used Path = " << (50 - path_size) << ", Used Time = " << delTime.count()*1000 << endl;
+          // cout << "Delta T by used Samples = " << delTime.count() / (50 - path_size) << endl;
 
-          
-          // cout << "Close X = " << map_waypoints_x[WP_N] << ", Close Y = " << map_waypoints_y[WP_N] << endl;
-
-          // for (int i = WP_N; i < WP_N + 50; ++i){
-          //   next_x_vals.push_back(map_waypoints_x[i]);
-          //   next_y_vals.push_back(map_waypoints_y[i]);
-          // }
-
-          // double pos_x;
-          // double pos_y;
-          // double angle;
-          // int path_size = previous_path_x.size();
-          // std::cout << "previous Path_size = " << path_size << std::endl;
-
-          // for (int i = 0; i < path_size; ++i){
-          //   next_x_vals.push_back(previous_path_x[path_size -1]);
-          //   next_y_vals.push_back(previous_path_y[path_size -1]);
-          // }
-
-          // if (path_size == 0){
-          //   pos_x = car_x;
-          //   pos_y = car_y;
-          //   angle = deg2rad(car_yaw);
-          // } else {
-          //   pos_x = previous_path_x[path_size -1];
-          //   pos_y = previous_path_y[path_size -1];
-          //   double pos_x2 = previous_path_x[path_size -2];
-          //   double pos_y2 = previous_path_y[path_size -2];
-          //   angle = atan2(pos_y - pos_y2, pos_x - pos_x2);
-          // }
-          
-          // double dist_inc = 0.5;
-          // for (int i = 0; i < 50; ++i){
-          //   next_x_vals.push_back(pos_x + (dist_inc)*cos(angle +(i+1)*(pi()/100)));
-          //   next_y_vals.push_back(pos_y + (dist_inc)*sin(angle +(i+1)*(pi()/100)));
-          //   pos_x += (dist_inc)*cos(angle +(i+1)*(pi()/100));
-          //   pos_y += (dist_inc)*sin(angle +(i+1)*(pi()/100));
-          // }
-
-          oldSpeed = car_speed; 
+          car_speed_old = car_speed; 
+          index++ ;
           /* End of TODO */
 
           msgJson["next_x"] = next_x_vals;
